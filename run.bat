c79037449dd63a92ca9242d791c229d5ed6914c1
KeerthanaSudar <138873556+KeerthanaSudar@users.noreply.github.com>
2023-07-10 17:43:25 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
-pytest -m "sanity" --html=reports/login_report.html testcases/ --browser=chrome
+REM pytest -m "sanity" --html=reports/login_report.html testcases/ --browser=chrome
+python -m pytest testcases/test_login.py --browser=chrome --html=reports/login_report.html
 REM pytest -m "regression" --html=reports/login_report.html testcases/ --browser=chrome
 REM python -m "sanity or regression" --html=reports/login_report.html testcases/ --browser=chrome
 REM python -m "sanity and regression" --html=reports/login_report.html testcases/ --browser=chrome
